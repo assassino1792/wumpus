aea0e6ec06c815aa2c3e30583dbf98a5158fc604
assassino1792 <103066469+assassino1792@users.noreply.github.com>
2023-12-30 13:37:13 +0100

    Hero és Leaderboard checkstyle

--- menu/menu/src/main/java/org/example/game/Hero.java
+++ menu/menu/src/main/java/org/example/game/Hero.java
@@ -6,35 +6,85 @@ import org.example.map.MapValidator;
 
 public class Hero {
 
-
+    /** The current map ID of the hero. */
     private MapID mapID;
+    /** The current way or direction the hero is facing. */
     private WayType way;
+    /** The number of arrows the hero has. */
     private int arrowCount;
 
+    /**
+     * Initializes a new instance of the Hero class.
+     */
+    public Hero() {
+    }
 
-    public Hero() {}
+    /**
+     * Gets the current way or direction the hero is facing.
+     * @return The current way of the hero.
+     */
     public WayType getWay() {
         return way;
     }
-    public void setWay(WayType way) {
-        this.way = way;
+
+    /**
+     * Sets the current newWay or direction the hero is facing.
+     * @param newWay The new newWay to set for the hero.
+     */
+    public void setWay(final WayType newWay) {
+        this.way = newWay;
     }
+    /**
+     * Gets the number of arrows the hero has.
+     * @return The number of arrows the hero has.
+     */
     public int getArrowCount() {
         return arrowCount;
     }
-    public void setArrowCount(int arrowCount) {
-        this.arrowCount = arrowCount;
+    /**
+     * Sets the number of arrows the hero has.
+     * @param newArrowCount The new number of arrows to set for the hero.
+     */
+    public void setArrowCount(final int newArrowCount) {
+        this.arrowCount = newArrowCount;
     }
+    /**
+     * Gets the current map ID of the hero.
+     * @return The current map ID of the hero.
+     */
     public MapID getMapID() {
         return mapID;
     }
-    public void setMapID(MapID mapID) {
-        this.mapID = mapID;
+    /**
+     * Sets the current map ID of the hero.
+     * @param newMapID The new map ID to set for the hero.
+     */
+    public void setMapID(final MapID newMapID) {
+        this.mapID = newMapID;
     }
+    /** Indicates whether the hero has gold. */
     private boolean hasGold = false;
-    public boolean isHasGold() { return hasGold; }
-    public void setHasGold(boolean hasGold) {this.hasGold = hasGold;}
-    public void initializeHero(int mapSize) {
+    /**
+     * Checks if the hero has gold.
+     * @return true if the hero has gold, false otherwise.
+     */
+    public boolean isHasGold() {
+        return hasGold;
+    }
+    /**
+     * Sets whether the hero has gold.
+     * @param newHasGold true if the hero has gold, false otherwise.
+     */
+    public void setHasGold(final boolean newHasGold) {
+        this.hasGold = newHasGold;
+    }
+    /**
+     * Initializes the hero with the specified
+     * number of arrows and facing north.
+     * @param mapSize The size of the map to
+     * determine the initial number of arrows.
+     */
+    public void initializeHero(final int mapSize) {
         this.arrowCount = MapValidator.WumpusCount(mapSize);
         this.way = WayType.NORTH;
     }
--- menu/menu/src/main/java/org/example/game/LeaderBoard.java
+++ menu/menu/src/main/java/org/example/game/LeaderBoard.java
@@ -1,10 +1,31 @@
 package org.example.game;
 
 import org.example.database.DatabaseService;
+
+/**
+ * A class representing the leaderboard for the game.
+ */
 public class LeaderBoard {
+    /** The database service for leaderboard data storage. */
     private DatabaseService dbService;
-    public LeaderBoard(DatabaseService dbService) {this.dbService = dbService;}
-    public void updateLeaderboard(String playerName, int steps, boolean hasWon ) {
+    /**
+     * Initializes a new instance of the LeaderBoard class.
+     * @param databaseService The database service
+     * used for leaderboard data storage.
+     */
+    public LeaderBoard(final DatabaseService databaseService) {
+        this.dbService = databaseService;
+    }
+    /**
+     * Updates the leaderboard with the player's information.
+     * @param playerName The name of the player.
+     * @param steps The number of steps taken by the player.
+     * @param hasWon Indicates whether the player has won the game.
+     */
+    public void updateLeaderboard(
+            final String playerName,
+            final int steps,
+            final boolean hasWon) {
         dbService.insertOrUpdateLeaderboard(playerName, steps, hasWon);
     }
 
