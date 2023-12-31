2653b1260cc5a7b3d0d5a3a91533056a8a37a29f
assassino1792 <103066469+assassino1792@users.noreply.github.com>
2023-12-31 17:18:18 +0100

    teszt eset megírása

---
+++ menu/menu/src/main/java/test/DatabaseConnectionTest.java
@@ -0,0 +1,107 @@
+package test;
+
+import org.example.database.DatabaseConnection;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseConnectionTest {
+
+    private DatabaseConnection databaseConnection;
+
+    @BeforeEach
+    void setUp() {
+        databaseConnection = new DatabaseConnection();
+    }
+
+    @AfterEach
+    void tearDown() {
+        databaseConnection.close();
+    }
+
+    @Test
+    void testInitializeDatabase() {
+
+        // Ellenőrizd, hogy a szükséges táblák létrejönnek-e
+
+        assertTrue(checkTableExists("PlayerNames"));
+
+        assertTrue(checkTableExists("Leaderboard"));
+
+        assertTrue(checkTableExists("GameState"));
+    }
+
+
+    @Test
+    void testDatabaseConnection() {
+        // Teszteld az adatbáziskapcsolatot
+        // Ellenőrizd, hogy a kapcsolat nem null és a helyes adatbázishoz kapcsolódik
+
+        Connection connection = databaseConnection.getConnection();
+
+        assertNotNull(connection);
+        // Ellenőrizd, hogy a kapcsolat létezik és nyitott
+        assertTrue(connectionIsValid(connection));
+    }
+
+
+
+    // Segédfüggvény az adott tábla létezésének ellenőrzéséhez
+    private boolean checkTableExists(String tableName) {
+        // Implementáld a tábla létezés ellenőrzését a konkrét adatbázisodhoz megfelelően
+        // Példa: SQL lekérdezés az adott tábla létezésének ellenőrzéséhez
+        String sql = "SHOW TABLES LIKE ?";
+        try (Connection connection = databaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, tableName);
+            return preparedStatement.executeQuery().next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Segédfüggvény az adatok beszúrásához a Leaderboard táblába tesztekhez
+    private void insertTestDataIntoLeaderboard() {
+        // Implementáld az adatok beszúrását a Leaderboard táblába tesztekhez
+        // Példa: SQL lekérdezés az adatok beszúrásához
+        String sql = "INSERT INTO Leaderboard (PlayerName, Steps, Wins) VALUES (?, ?, ?)";
+        try (Connection connection = databaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, "Player1");
+            preparedStatement.setInt(2, 100);
+            preparedStatement.setInt(3, 2);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.setString(1, "Player2");
+            preparedStatement.setInt(2, 150);
+            preparedStatement.setInt(3, 1);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.setString(1, "Player3");
+            preparedStatement.setInt(2, 200);
+            preparedStatement.setInt(3, 3);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Segédfüggvény az adatbázis kapcsolat ellenőrzéséhez
+    private boolean connectionIsValid(Connection connection) {
+        // Implementáld a kapcsolat ellenőrzését a konkrét adatbázisodhoz megfelelően
+        // Példa: Ellenőrizd, hogy a kapcsolat létezik és nyitott-e
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ menu/menu/src/main/java/test/GameTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import org.example.game.GamePlay;
+import org.example.game.Hero;
+import org.example.map.MapID;
+import org.example.map.MapReader;
+import org.example.map.WayType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class GameTest {
+
+    private GamePlay gamePlay;
+    private Hero hero;
+    private MapReader mapReader;
+
+    @BeforeEach
+    void setUp() {
+        hero = new Hero();
+        mapReader = new MapReader();
+        assertTrue(mapReader.readMapFromFile()); // Inicializálja a térképet és a hős kezdeti pozícióját
+        hero.setMapID(mapReader.getHeroInitialPosition()); // Beállítja a hős kezdeti pozícióját
+        hero.setWay(WayType.NORTH); // Beállítja a hős irányát
+        gamePlay = new GamePlay(hero, mapReader, 0, 0, "TestPlayer");
+    }
+
+    @Test
+    void testInitialHeroPosition() {
+        // Ellenőrizze, hogy a hős kezdeti pozíciója helyes-e
+        assertEquals(mapReader.getHeroInitialPosition(), hero.getMapID());
+    }
+
+    @Test
+    void testHeroMovement() {
+        // Mozgassa a hőst és ellenőrizze az új pozíciót
+        gamePlay.changeHeroDirection(WayType.NORTH);
+        gamePlay.moveHero();
+        // Feltételezve, hogy a kezdeti pozíció (3, 8) és a térkép mérete megengedi a mozgást
+        assertEquals(new MapID(3, 7), hero.getMapID()); // Feltételezve, hogy északra mozog
+    }
+
+    @Test
+    void testPickUpGold() {
+        // Feltételezve, hogy az arany a hős kezdeti pozícióján nincs és nem tudja felvenni
+        gamePlay.pickUpGold();
+        assertFalse(hero.isHasGold());
+    }
+
+    @Test
+    void testWinningGame() {
+        // Feltételezve, hogy a hős felvette az aranyat és visszatér a kezdeti pozícióba
+        hero.setHasGold(true);
+        // Mozgassa a hőst a kezdeti pozícióba
+        gamePlay.moveHero();
+        hero.setMapID(new MapID(3, 8));
+        assertTrue(gamePlay.hasWon());
+    }
+}
+
